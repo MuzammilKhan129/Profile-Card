b4eabb81d8527c82b29cd5b22ff508c918d5fa71
moni29 <muzammilkhan129eb@gmail.com>
2025-08-23 04:25:06 +0500

    main

--- src/index.js
+++ src/index.js
@@ -1,17 +1,70 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { StrictMode } from "react";
+import ReactDom from "react-dom/client";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+function App() {
+  return (
+    <div className="card">
+      <Avatar />
+      <div className="data">
+        <Intro />
+        {/* Should contain one Skill component
+            for each web dev skill that you have,
+            customized with props */}
+        <SkillList />
+      </div>
+    </div>
+  );
+}
+function Intro() {
+  return (
+    <div>
+      <h1>Muhammad Muzammil Ijaz</h1>
+      <p>
+        Frontend Developer and Tech lover. When I'm not coding, I like to watch
+        some football or Cricket. I'm from Arifwala,Punjab(Pakistan) but living
+        in BahawalPur(Pakistan).
+      </p>
+    </div>
+  );
+}
+
+function Avatar() {
+  return (
+    <div>
+      <img
+        className="avatar"
+        src={`${process.env.PUBLIC_URL}/images/moni.jpg`}
+        alt="Muzammil Ijaz"
+      />
+    </div>
+  );
+}
+
+function SkillList() {
+  return (
+    <div className="skill-list">
+      <Skill skill="React" emoji="✌" color="lightblue" />
+      <Skill skill="HTML+CSS" emoji="🎉" color="orange" />
+      <Skill skill="JavaScript" emoji="✔" color="yellow" />
+      <Skill skill="Python" emoji="👌" color="lightgrey" />
+    </div>
+  );
+}
+function Skill(props) {
+  return (
+    <div className="skill" style={{ backgroundColor: props.color }}>
+      <span>
+        {props.skill} {props.emoji}
+      </span>
+    </div>
+  );
+}
+
+const root = ReactDom.createRoot(document.getElementById("root"));
+
 root.render(
-  <React.StrictMode>
+  <StrictMode>
     <App />
-  </React.StrictMode>
+  </StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
